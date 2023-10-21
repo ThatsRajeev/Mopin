e8374357d58b443673a59b613275777daf766e80
Rajeev <rajeev21c@outlook.com>
2023-10-21 08:10:59 +0530

    Connecting to server

--- server/index.js
+++ server/index.js
@@ -16,6 +16,12 @@ app.use(cors({
   methods: ["POST", "GET"],
   credentials: true,
 }));
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
+
 dotenv.config();
 
 app.use(express.json());
