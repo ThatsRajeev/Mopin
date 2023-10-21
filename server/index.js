4786cbf0bfce0c3bf5b1995e78004a9defee0905
Rajeev <rajeev21c@outlook.com>
2023-10-21 08:13:57 +0530

    Connecting to server

--- server/index.js
+++ server/index.js
@@ -18,6 +18,7 @@ app.use(cors({
 }));
 
 app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://mopin-frontend.vercel.app');
   res.setHeader('Access-Control-Allow-Credentials', true);
   next();
 });
