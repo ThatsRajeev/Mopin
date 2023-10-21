1f54526bf8a5d50fd4dbe17a8e1754958ea34784
Rajeev <rajeev21c@outlook.com>
2023-10-21 09:51:04 +0530

    Connecting to server

--- server/index.js
+++ server/index.js
@@ -18,6 +18,24 @@ app.use(cors({
   credentials: true,
 }));
 
+app.use(function (req, res, next) {
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'https://mopin-frontend.vercel.app');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 app.use('/proxy', function(req, res) {
   var url = 'https://api.opencagedata.com' + req.url;
   req.pipe(request(url)).pipe(res);
