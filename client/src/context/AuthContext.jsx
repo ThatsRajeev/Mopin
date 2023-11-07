94c1ab4a47e0b6f359d33245df91ead48e245e16
Rajeev <rajeev21c@outlook.com>
2023-11-07 11:19:04 +0530

    bug fixes

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -22,7 +22,6 @@ export const UserAuthContextProvider = ({ children }) => {
         "size": "invisible",
       }
     );
-    recaptchaVerifier.render();
     return signInWithPhoneNumber(auth, number, recaptchaVerifier);
   }
 
