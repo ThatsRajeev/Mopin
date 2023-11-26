86fdb8dba5c6a1c00cb716b6517bac3fb19e14e4
Rajeev <rajeev21c@outlook.com>
2023-11-26 10:16:15 +0530

    Code Refactoring

--- client/src/components/Location/Location.jsx
+++ client/src/components/Location/Location.jsx
@@ -1,23 +1,21 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import handleGPS from "../../utils/handleGPS";
+import "./Location.css";
 
 const Location = ({ setAdrsProp, setShowProp }) => {
   const [inputValue, setInputValue] = useState("");
   const [suggestions, setSuggestions] = useState([]);
 
   const getCurrentLocation = async () => {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(async (position) => {
-        const { latitude, longitude } = position.coords;
-        const response = await axios.get(
-          `https://mopin-server.vercel.app/proxy/geocode/v1/json?q=${latitude}+${longitude}&key=12b6daa5213d46898ef052dfacf9ac5a`,
-          { withCredentials: false }
-        );
-        setAdrsProp(response.data.results[0].formatted);
+    try {
+      if (navigator.geolocation) {
         setShowProp('address');
-      });
-    } else {
-      console.error("Geolocation is not supported by this browser.");
+        const res = await handleGPS();
+        setAdrsProp(res.results[0].formatted);
+      }
+    } catch (e) {
+      console.error(e);
     }
   };
 
@@ -52,15 +50,13 @@ const Location = ({ setAdrsProp, setShowProp }) => {
 
   return (
     <>
-      <div className="mob-view">
-        <div style={{display: 'flex', marginTop: '28px', fontSize: '16px'}}>
-          <span class="material-symbols-outlined" style={{marginRight: '16px'}} onClick={() => setShowProp('address')}>arrow_back</span>
-          <p> Search your delivery location</p>
-        </div>
+      <div className="search-heading mob-view">
+        <span class="material-symbols-outlined" onClick={() => setShowProp('address')}>arrow_back</span>
+        <p> Search your delivery location</p>
       </div>
       <div className="address-container">
         <div className="address-div">
-          <div><span style={{display: 'flex', marginRight: '4px', opacity: '64%'}} className="material-symbols-outlined">search</span></div>
+          <span className="material-symbols-outlined">search</span>
           <input
             className="address-input"
             placeholder="Search for locality or street name"
@@ -69,11 +65,10 @@ const Location = ({ setAdrsProp, setShowProp }) => {
           />
           {inputValue && (
             <button className="clear-button" onClick={handleClearInput}>
-            <span class="material-symbols-outlined" style={{fontSize: '16px', margin: '0'}}>close</span>
+              <span class="material-symbols-outlined">close</span>
             </button>
           )}
         </div>
-
       </div>
       <ul className="suggUL">
         {suggestions.length !== 0 && (
@@ -84,8 +79,7 @@ const Location = ({ setAdrsProp, setShowProp }) => {
                 key={suggestion.annotations.geohash}
                 onClick={() => handleSuggestionClick(suggestion)}
               >
-
-              <span class="material-symbols-outlined" style={{marginRight: '16px'}}>location_on</span>
+                <span class="material-symbols-outlined location-icon">location_on</span>
                 <div className="SuggDiv">
                   <p>
                     {
@@ -106,7 +100,7 @@ const Location = ({ setAdrsProp, setShowProp }) => {
         Use Current Location Using GPS
       </p>
       <div className="choose-location">
-        <div className="locationIcon-container">
+        <div className="location-icon-container">
           <span class="material-symbols-outlined location-on-icon">location_on</span>
         </div>
         <p className="choose-location-p1"> Choose your location! </p>
