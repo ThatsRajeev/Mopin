4d90f6237ae8f663ebf39c69b9fff09542453bfc
Rajeev <rajeev21c@outlook.com>
2024-01-09 18:49:18 +0530

    App Creation

--- client/src/components/Location/Location.jsx
+++ client/src/components/Location/Location.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import handleGPS from "../../utils/handleGPS";
 import "./Location.css";
