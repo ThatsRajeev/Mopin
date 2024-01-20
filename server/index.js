324348956537142246c700c7fcf2bafb2822919f
Rajeev <rajeev21c@outlook.com>
2024-01-20 11:02:58 +0530

    App Creation

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 
 app.use(cors({
-  origin: "https://mopin-frontend.vercel.app",
+  origin: ["https://mopin-frontend.vercel.app", "http://localhost:3000"],
   methods: ["POST", "GET"],
   credentials: true,
 }));
