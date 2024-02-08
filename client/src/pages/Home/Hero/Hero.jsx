a3523678e744006175d278c8e669e35a7f7367da
Rajeev <rajeev21c@outlook.com>
2024-02-08 14:08:56 +0530

    PWA

--- client/src/pages/Home/Hero/Hero.jsx
+++ client/src/pages/Home/Hero/Hero.jsx
@@ -27,7 +27,7 @@ function Hero() {
         </div>
       </div>
       <div className="hero-smallScreen-div" onClick={scroll}>
-        <img fetchpriority="high" className="hero-smallScreen skeleton mob-view" src="https://mopin-assets.s3.ap-south-1.amazonaws.com/base+images/Group+181+(1).webp" alt="food_img" />
+        <img fetchpriority="high" className="hero-smallScreen mob-view" src="https://mopin-assets.s3.ap-south-1.amazonaws.com/base+images/Group+181+(1).webp" alt="food_img" />
       </div>
     </>
   );
--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -19,26 +19,8 @@ const Footer = lazy(() => import("../../components/Footer/Footer"));
 function Homepage() {
   const [locationPrompt, setLocationPrompt] = useState(false);
   const [active, setActive] = useState('All');
-  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
   const foodCatgor = ['All', 'Spicy', 'Veg', 'Non-veg', 'Dairy-free'];
-
-  useEffect(() => {
-    toast("This app currently only serves test data. We will be live soon!", {
-      duration: 4000,
-      id: 'soon',
-      icon: '👨‍💻'
-    });
-
-    const handleResize = () => {
-      setWindowWidth(window.innerWidth);
-    };
-
-    window.addEventListener("resize", handleResize);
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
 
   useEffect(() => {
     const checkLocationPermission = async () => {
@@ -88,111 +70,104 @@ function Homepage() {
         <>
           <Suspense fallback={<SkeletonCard />}>
             <Navbar />
-            <Toaster
-              position="bottom-center"
-            />
-
             <Hero />
-            {windowWidth < 768 ? (
-              <>
-                <div className="foodCatgor">
-                  {foodCatgor.map((catgor, index) => (
-                    <div key={index} className={`${active === catgor ? "active-catgor" : ""}`}
-                      onClick={() => setActive(catgor)}>{catgor}</div>
-                  ))}
-                </div>
-                <FoodSlider func={filterDish} />
-                <hr />
 
-                <div className="header-container">
-                  <h1 className="cardHeader">All Homechefs Nearby</h1>
-                </div>
-                {homecooks.map((cook, index) => (
-                  <Link to ={`/sellers/${cook.name}`} style={{textDecoration: 'none'}}>
-                    <ChefCard
-                      key={index}
-                      id={index}
-                      name={cook.name}
-                      img={cook.imgURL}
-                      foodType={cook.foodType}
-                      rating={cook.rating}
-                      noOfOrders={cook.noOfOrders}
-                      minPrice={cook.minPrice}
-                      className = "card"
-                    />
-                    </Link>
+            <div className="mob-view">
+              <div className="foodCatgor">
+                {foodCatgor.map((catgor, index) => (
+                  <div key={index} className={`${active === catgor ? "active-catgor" : ""}`}
+                    onClick={() => setActive(catgor)}>{catgor}</div>
                 ))}
+              </div>
+              <FoodSlider func={filterDish} />
+              <hr />
 
-                <img className="hero-smallScreen" src="https://mopin-assets.s3.ap-south-1.amazonaws.com/base+images/Group-182.avif" alt="footer_img" />
-              </>
-            ) : (
-              <>
+              <div className="header-container">
+                <h1 className="cardHeader">All Homechefs Nearby</h1>
+              </div>
+              {homecooks.map((cook, index) => (
+                <Link to ={`/sellers/${cook.name}`} style={{textDecoration: 'none'}}>
+                  <ChefCard
+                    key={index}
+                    id={index}
+                    name={cook.name}
+                    img={cook.imgURL}
+                    foodType={cook.foodType}
+                    rating={cook.rating}
+                    noOfOrders={cook.noOfOrders}
+                    minPrice={cook.minPrice}
+                    className = "card"
+                  />
+                  </Link>
+              ))}
+            </div>
+
+            <div className="pc-view">
+              <div className="header-container">
+                <h1 className="cardHeader">Added Afresh</h1>
+                <button className="see-all-btn">
+                  See All Recently Added
+                  <span className="material-symbols-outlined">
+                    navigate_next
+                  </span>
+                </button>
+              </div>
+
+              <FoodSlider func={newly} />
+              <hr />
+
+              <div className="header-container">
+                <h1 className="cardHeader">Popular Regionals</h1>
+              </div>
+              <Fooder />
+
+              <div className="feature-bcg pc-view">
                 <div className="header-container">
-                  <h1 className="cardHeader">Added Afresh</h1>
-                  <button className="see-all-btn">
-                    See All Recently Added
-                    <span className="material-symbols-outlined">
-                      navigate_next
-                    </span>
-                  </button>
+                  <h1 className="cardHeader">
+                    Grab your <span style={{ color: "#f16122" }}>ORDER NOW!</span>
+                  </h1>
                 </div>
+                <Features />
+              </div>
 
-                <FoodSlider func={newly} />
-                <hr />
+              <div className="header-container">
+                <h1 className="cardHeader">Most Loved near you</h1>
+                <button className="see-all-btn">
+                  See All Loved Collections
+                  <span className="material-symbols-outlined">
+                    navigate_next
+                  </span>
+                </button>
+              </div>
 
-                <div className="header-container">
-                  <h1 className="cardHeader">Popular Regionals</h1>
-                </div>
-                <Fooder />
+              <FoodSlider func={rated} />
+              <hr />
 
-                <div className="feature-bcg pc-view">
-                  <div className="header-container">
-                    <h1 className="cardHeader">
-                      Grab your <span style={{ color: "#f16122" }}>ORDER NOW!</span>
-                    </h1>
-                  </div>
-                  <Features />
-                </div>
+              <div className="header-container">
+                <h1 className="cardHeader">Healthy Picks</h1>
+                <button className="see-all-btn">
+                  See All Healthy Picks
+                  <span className="material-symbols-outlined">
+                    navigate_next
+                  </span>
+                </button>
+              </div>
 
-                <div className="header-container">
-                  <h1 className="cardHeader">Most Loved near you</h1>
-                  <button className="see-all-btn">
-                    See All Loved Collections
-                    <span className="material-symbols-outlined">
-                      navigate_next
-                    </span>
-                  </button>
-                </div>
+              <FoodSlider func={healthy} />
+              <hr />
 
-                <FoodSlider func={rated} />
-                <hr />
+              <div className="header-container">
+                <h1 className="cardHeader">Meet the Makers</h1>
+              </div>
+              <Makers />
 
-                <div className="header-container">
-                  <h1 className="cardHeader">Healthy Picks</h1>
-                  <button className="see-all-btn">
-                    See All Healthy Picks
-                    <span className="material-symbols-outlined">
-                      navigate_next
-                    </span>
-                  </button>
-                </div>
+              <div className="header-container pc-view">
+                <h1 className="cardHeader">What do our Customers have to say</h1>
+              </div>
+              <Testimonials />
 
-                <FoodSlider func={healthy} />
-                <hr />
-
-                <div className="header-container">
-                  <h1 className="cardHeader">Meet the Makers</h1>
-                </div>
-                <Makers />
-
-                <div className="header-container pc-view">
-                  <h1 className="cardHeader">What do our Customers have to say</h1>
-                </div>
-                <Testimonials />
-
-                <Footer />
-              </>
-            )}
+              <Footer />
+            </div>
           </Suspense>
         </>
       )}
