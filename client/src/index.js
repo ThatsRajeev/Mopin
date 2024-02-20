810cda98442e480af7e1b13df69470e30c3fd124
Rajeev <rajeev21c@outlook.com>
2024-02-20 10:20:49 +0530

    redux toolkit

--- client/src/index.js
+++ client/src/index.js
@@ -2,12 +2,16 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import { UserAuthContextProvider } from "./context/AuthContext";
+import { store } from "./store/index";
+import { Provider } from "react-redux";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <UserAuthContextProvider>
-      <App />
-    </UserAuthContextProvider>
+    <Provider store={store}>
+      <UserAuthContextProvider>
+        <App />
+      </UserAuthContextProvider>
+    </Provider>
   </React.StrictMode>
 );
---
+++ client/src/store/productsSlice.js
@@ -0,0 +1,37 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+
+export const fetchProducts = createAsyncThunk(
+  'products/fetchProducts',
+  async () => {
+    const response = await fetch ('http://localhost:8080/homemakers');
+    return response.json();
+  }
+);
+
+const productsSlice = createSlice({
+  name: 'products',
+  initialState: {
+    items: [],
+    status: 'idle',
+    error: null,
+  },
+  reducers: {
+    // We will add some reducers to handle things like filtering
+  },
+  extraReducers(builder) {
+    builder
+      .addCase(fetchProducts.pending, (state, action) => {
+        state.status = 'loading';
+      })
+      .addCase(fetchProducts.fulfilled, (state, action) => {
+        state.status = 'succeeded';
+        state.items = action.payload;
+      })
+      .addCase(fetchProducts.rejected, (state, action) => {
+        state.status = 'failed';
+        state.error = action.error.message;
+      });
+  },
+});
+
+export default productsSlice.reducer;
