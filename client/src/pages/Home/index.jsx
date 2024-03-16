e5e9a0736b9e3217ee84d73ae367ecb4800a2bdb
Rajeev <rajeev21c@outlook.com>
2024-03-16 12:11:09 +0530

    minor fixes

--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -1,20 +1,21 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, lazy, Suspense } from "react";
 import { toast, Toaster } from "react-hot-toast";
 import { Link } from "react-router-dom";
-import SkeletonCard from "./skeleton.jsx";
+import SkeletonCard from "./skeleton.jsx"
 import LocateMePrompt from "../../components/LocateMePrompt/LocateMePrompt";
-import Hero from "./Hero/Hero";
+import Navbar from "../../components/Navbar/Navbar";
 import homecooks from "../../data/homecooks";
 import "leaflet/dist/leaflet.css";
-import Navbar from "../../components/Navbar/Navbar";
-import FoodSlider from "./FoodSlider/FoodSlider";
-import FoodItemCard from "./FoodItemCard/FoodItemCard";
-import MealtimeFilter from "./MealtimeFilter/MealtimeFilter";
-import Features from "./Features/Features";
-import Fooder from "./Fooder/Fooder";
-import Makers from "./Makers/Makers";
-import Testimonials from "./Testimonials/Testimonials";
-import Footer from "../../components/Footer/Footer";
+
+const Hero = lazy(() => import("./Hero/Hero"));
+const FoodSlider = lazy(() => import("./FoodSlider/FoodSlider"));
+const FoodItemCard = lazy(() => import("./FoodItemCard/FoodItemCard"));
+const MealtimeFilter = lazy(() => import("./MealtimeFilter/MealtimeFilter"));
+const Features = lazy(() => import("./Features/Features"));
+const Fooder = lazy(() => import("./Fooder/Fooder"));
+const Makers = lazy(() => import("./Makers/Makers"));
+const Testimonials = lazy(() => import("./Testimonials/Testimonials"));
+const Footer = lazy(() => import("../../components/Footer/Footer"));
 
 function Homepage() {
   const [locationPrompt, setLocationPrompt] = useState(false);
@@ -69,108 +70,110 @@ function Homepage() {
       ) : (
         <>
           <Navbar />
-          <Hero />
+          <Suspense fallback={<SkeletonCard />}>
+            <Hero />
 
-          <div className="mob-view">
-            <div className="foodCatgor">
-              {foodCatgor.map((catgor, index) => (
-                <div key={index} className={`${active === catgor ? "active-catgor" : ""}`}
-                  onClick={() => setActive(catgor)}>{catgor}</div>
-              ))}
-            </div>
-            <FoodSlider func={filterDish} />
-            <hr />
-
-            <MealtimeFilter />
-
-            <div className="header-container">
-              <h1 className="cardHeader">All Homechefs Nearby</h1>
-            </div>
-            {homecooks.map((cook, index) => (
-              <Link to ={`/sellers/${cook.name}`} style={{textDecoration: 'none'}}>
-                <FoodItemCard
-                  key={index}
-                  cardType={"chef"}
-                  name={cook.name}
-                  img={cook.imgURL}
-                  foodType={cook.foodType}
-                  rating={cook.rating}
-                  feeds={cook.feeds}
-                  noOfOrders={cook.noOfOrders}
-                  minPrice={cook.minPrice}
-                />
-                </Link>
-            ))}
-
-            <img className="hero-smallScreen" src="https://mopin-assets.s3.ap-south-1.amazonaws.com/base+images/Group-182.avif" alt="footer_img" />
-          </div>
-
-          <div className="pc-view" style={{flexDirection: 'column'}}>
-            <div className="header-container">
-              <h1 className="cardHeader">Added Afresh</h1>
-              <button className="see-all-btn">
-                See All Recently Added
-                <span className="material-symbols-outlined">
-                  navigate_next
-                </span>
-              </button>
-            </div>
-
-            <FoodSlider func={newly} />
-            <hr />
-
-            <div className="header-container">
-              <h1 className="cardHeader">Popular Regionals</h1>
-            </div>
-            <Fooder />
-
-            <div className="feature-bcg">
-              <div className="header-container">
-                <h1 className="cardHeader" style={{margin: '0 auto'}}>
-                  Grab your <span style={{ color: "#f16122" }}>ORDER NOW!</span>
-                </h1>
+            <div className="mob-view">
+              <div className="foodCatgor">
+                {foodCatgor.map((catgor, index) => (
+                  <div key={index} className={`${active === catgor ? "active-catgor" : ""}`}
+                    onClick={() => setActive(catgor)}>{catgor}</div>
+                ))}
               </div>
-              <Features />
+              <FoodSlider func={filterDish} />
+              <hr />
+
+              <MealtimeFilter />
+
+              <div className="header-container">
+                <h1 className="cardHeader">All Homechefs Nearby</h1>
+              </div>
+              {homecooks.map((cook, index) => (
+                <Link to ={`/sellers/${cook.name}`} style={{textDecoration: 'none'}}>
+                  <FoodItemCard
+                    key={index}
+                    cardType={"chef"}
+                    name={cook.name}
+                    img={cook.imgURL}
+                    foodType={cook.foodType}
+                    rating={cook.rating}
+                    feeds={cook.feeds}
+                    noOfOrders={cook.noOfOrders}
+                    minPrice={cook.minPrice}
+                  />
+                  </Link>
+              ))}
+
+              <img className="hero-smallScreen" loading="lazy" src="https://mopin-assets.s3.ap-south-1.amazonaws.com/base+images/Group-182.avif" alt="footer_img" />
             </div>
 
-            <div className="header-container">
-              <h1 className="cardHeader">Most Loved near you</h1>
-              <button className="see-all-btn">
-                See All Loved Collections
-                <span className="material-symbols-outlined">
-                  navigate_next
-                </span>
-              </button>
+            <div className="pc-view" style={{flexDirection: 'column'}}>
+              <div className="header-container">
+                <h1 className="cardHeader">Added Afresh</h1>
+                <button className="see-all-btn">
+                  See All Recently Added
+                  <span className="material-symbols-outlined">
+                    navigate_next
+                  </span>
+                </button>
+              </div>
+
+              <FoodSlider func={newly} />
+              <hr />
+
+              <div className="header-container">
+                <h1 className="cardHeader">Popular Regionals</h1>
+              </div>
+              <Fooder />
+
+              <div className="feature-bcg">
+                <div className="header-container">
+                  <h1 className="cardHeader" style={{margin: '0 auto'}}>
+                    Grab your <span style={{ color: "#f16122" }}>ORDER NOW!</span>
+                  </h1>
+                </div>
+                <Features />
+              </div>
+
+              <div className="header-container">
+                <h1 className="cardHeader">Most Loved near you</h1>
+                <button className="see-all-btn">
+                  See All Loved Collections
+                  <span className="material-symbols-outlined">
+                    navigate_next
+                  </span>
+                </button>
+              </div>
+
+              <FoodSlider func={rated} />
+              <hr />
+
+              <div className="header-container">
+                <h1 className="cardHeader">Healthy Picks</h1>
+                <button className="see-all-btn">
+                  See All Healthy Picks
+                  <span className="material-symbols-outlined">
+                    navigate_next
+                  </span>
+                </button>
+              </div>
+
+              <FoodSlider func={healthy} />
+              <hr />
+
+              <div className="header-container">
+                <h1 className="cardHeader">Meet the Makers</h1>
+              </div>
+              <Makers />
+
+              <div className="header-container">
+                <h1 className="cardHeader">What do our Customers have to say</h1>
+              </div>
+              <Testimonials />
+
+              <Footer />
             </div>
-
-            <FoodSlider func={rated} />
-            <hr />
-
-            <div className="header-container">
-              <h1 className="cardHeader">Healthy Picks</h1>
-              <button className="see-all-btn">
-                See All Healthy Picks
-                <span className="material-symbols-outlined">
-                  navigate_next
-                </span>
-              </button>
-            </div>
-
-            <FoodSlider func={healthy} />
-            <hr />
-
-            <div className="header-container">
-              <h1 className="cardHeader">Meet the Makers</h1>
-            </div>
-            <Makers />
-
-            <div className="header-container">
-              <h1 className="cardHeader">What do our Customers have to say</h1>
-            </div>
-            <Testimonials />
-
-            <Footer />
-          </div>
+          </Suspense>
         </>
       )}
     </>
--- client/src/pages/Home/skeleton.jsx
+++ client/src/pages/Home/skeleton.jsx
@@ -2,42 +2,42 @@ import React from "react";
 import Skeleton from "react-loading-skeleton";
 
 const SkeletonCard = () => {
-    return (
-      <section className="skeleton-section">
-        <h2 className="skeleton-title">
-          <Skeleton height={36} width={`74vw`} />
-          <Skeleton circle={true} height={44} width={44} />
-        </h2>
+  return (
+    <section className="skeleton-section">
+      <h2 className="skeleton-title">
+        <Skeleton height={36} width={`74vw`} color="#eef0f5" />
+        <Skeleton circle={true} height={44} width={44} color="#eef0f5" />
+      </h2>
 
-        <Skeleton height={48} />
+      <Skeleton height={48} color="#eef0f5" />
 
-        <Skeleton height={180} />
+      <Skeleton height={180} color="#eef0f5" />
+      <h4 className="card-title">
+        <Skeleton height={36} color="#eef0f5" />
+      </h4>
+      <div className="skeletion-card">
+        <p className="card-channel">
+          <Skeleton width={`46vw`} height={180} color="#eef0f5" />
+        </p>
+        <p className="card-channel">
+          <Skeleton width={`40vw`} height={180} color="#eef0f5" />
+        </p>
+      </div>
+
+      <ul className="skeleton-list">
         <h4 className="card-title">
-          <Skeleton height={36} />
+          <Skeleton height={36} width={156} color="#eef0f5" />
         </h4>
-        <div className="skeletion-card">
-          <p className="card-channel">
-            <Skeleton width={`46vw`} height={180} />
-          </p>
-          <p className="card-channel">
-            <Skeleton width={`40vw`} height={180} />
-          </p>
-        </div>
-
-        <ul className="skeleton-list">
-          <h4 className="card-title">
-            <Skeleton height={36} width={156} />
-          </h4>
-          {Array(6)
-            .fill()
-            .map((item, index) => (
-              <li className="skeleton-title" key={index}>
-                <Skeleton circle={true} height={148} width={148} />
-                <Skeleton height={148} width={`44vw`}/>
-              </li>
-            ))}
-          </ul>
-      </section>
-    );
-  };
-  export default SkeletonCard;
+        {Array(6)
+          .fill()
+          .map((item, index) => (
+            <li className="skeleton-title" key={index}>
+              <Skeleton circle={true} height={148} width={148} color="#eef0f5" />
+              <Skeleton height={148} width={`44vw`} color="#eef0f5" />
+            </li>
+          ))}
+      </ul>
+    </section>
+  );
+};
+export default SkeletonCard;
